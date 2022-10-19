7dbe07b444900adf49764a7d2c5570af28f946fe
ERIKMAGDIEL <me@DESKTOP-2DUUIQK>
2022-10-18 20:44:02 -0500

    Agregar archivos de proyecto.

---
+++ String y sus métodos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_y_sus_métodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("Por favor, ingresa tu nombre y presiona Enter.");
+            nombre = Console.ReadLine();
+            
+
+            
+            Console.WriteLine(nombre.ToUpper());
+            Console.WriteLine(nombre.ToLower());
+            Console.WriteLine(nombre.Trim());
+            Console.WriteLine(nombre.Substring(4));
+
+            Console.ReadLine();
+            
+
+        }
+         
+        
+
+        }
+
+
+
+
+
+
+    
+}
